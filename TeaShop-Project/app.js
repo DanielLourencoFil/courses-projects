f7b4dac2047de0a5b41e2ea2e56ff7c80bd5a80d
daniellourencoedu <daniellourencoedu@gmail.com>
2022-01-30 20:03:48 -0300

    nav btn and back to top btn js; links to site pages added

--- TeaShop-Project/app.js
+++ TeaShop-Project/app.js
@@ -1,18 +1,36 @@
 //setup date
-const date = (document.getElementById(
-    "date"
-).innerHTML = new Date().getFullYear());
+const date = document.getElementById("date").innerHTML = new Date().getFullYear();
+
+
 // setup nav
 const navBtn = document.getElementById("nav-btn");
+const backTopBtn = document.getElementById("back-top-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+
 // show nav
 navBtn.addEventListener("click", () => {
+    console.log('oh');
     navbar.classList.add("showNav");
-});
+})
+
 // close nav
 navClose.addEventListener("click", () => {
     navbar.classList.remove("showNav");
 });
 
-document.getElementById("script").innerHTML = 'Hello World';
+// nav btn && back to top btn: hide on scroll
+
+window.addEventListener('scroll', (e)=>{
+    if(window.scrollY > 500){
+        navBtn.style.opacity = 0
+        backTopBtn.style.opacity = 1
+    }
+    else {
+        backTopBtn.style.opacity = 0
+        navBtn.style.opacity = 1
+    }
+})
+
+// back to top btn
+
